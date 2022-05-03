62c2ab44d81a7e5dbd346f666719edf2c52828ad
Achsuthan <achsuthan@icloud.com>
2022-05-03 16:21:09 +0800

    🔥 files added for service

---
+++ ___FILEBASENAME___/___FILEBASENAME___Service.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILEBASENAMEASIDENTIFIER___.swift
+//
+//  Created by CustomTemplates.
+//  Copyright © 2022 SPH. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+
+
+    // MARK: LifeCycle Methods
+    init {}
+    
+
+    deinit {}
+}
