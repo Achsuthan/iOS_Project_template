7b4eea55964dfad381fad8e3a4089e1b5f5125a6
Achsuthan <achsuthan@icloud.com>
2022-05-03 15:21:57 +0800

    🔥 basicfiles added

---
+++ __FILEBASENAME__/Constant/___FILEBASENAME__Constant.swift
@@ -0,0 +1,10 @@
+//
+//  ___FILEBASENAMEASIDENTIFIER___.swift
+//
+//  Created by CustomTemplates.
+//  Copyright © 2022 SPH. All rights reserved.
+//
+
+import Foundation
+
+struct ___FILEBASENAMEASIDENTIFIER___ {}
