edf0f07af9cac61e7f033cae78c4eb3eab22b0cd
Achsuthan <achsuthan@icloud.com>
2022-05-03 16:38:00 +0800

    🔥 coordinator file details added

---
+++ ___FILEBASENAME___/Coordinator/___FILEBASENAME___Coordinator.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILEBASENAMEASIDENTIFIER___.swift
+//  BTRevamp2019_iOS
+//
+//  Created by CustomTemplates.
+//  Copyright © 2022 SPH. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Coordinator
+
+class ArticleDetailCoordinator: BaseCoordinator {
+    
+    init() {}
+    
+    override func start() {}
+}
