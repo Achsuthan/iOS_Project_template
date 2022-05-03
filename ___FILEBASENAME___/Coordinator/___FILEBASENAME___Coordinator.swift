4a3e6425290f05102d97c0886fb9b64ebb012ed2
Achsuthan <achsuthan@icloud.com>
2022-05-03 16:40:00 +0800

    🐛 file name bug fixed

--- ___FILEBASENAME___/Coordinator/___FILEBASENAME___Coordinator.swift
+++ ___FILEBASENAME___/Coordinator/___FILEBASENAME___Coordinator.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 import Coordinator
 
-class ArticleDetailCoordinator: BaseCoordinator {
+class ___FILEBASENAMEASIDENTIFIER___: BaseCoordinator {
     
     init() {}
     
